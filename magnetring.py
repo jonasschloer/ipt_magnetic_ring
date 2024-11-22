23a387490bd340b1bcbd320d5771842c9cbf43ff
jonasschloer <jschloer@student.ethz.ch>
2024-11-22 09:53:34 +0100

    readme

--- magnetring.py
+++ magnetring.py
@@ -2,17 +2,21 @@ import numpy as np
 import argparse
 import cv2
 import matplotlib.pyplot as plt
-_ = plt.__path__
-_ = np.__path__
+_ = plt.get_cmap("Reds")
+_ = np.zeros(1)
 
 # Device index (4) on my machine with DroidCam
 device_index = 4
 # Points to track
 points_to_track = 5
 
-filter_range = [20, 2000]
+# For detecting boundaries
+contour_range = [20, 2000]
 # DEBUGGING:
-vary_filter_range = False
+vary_contour_range = False
+
+# For detecting dark regions
+threshold_range = [15, 255]
 
 
 def vary_range():
@@ -22,19 +26,19 @@ def vary_range():
     stepsize = 30
     rise = True
     if rise:
-        filter_range[index] += stepsize
+        contour_range[index] += stepsize
     else:
-        filter_range[index] -= stepsize
-    if filter_range[index] > maxval:
+        contour_range[index] -= stepsize
+    if contour_range[index] > maxval:
         rise = False
-    if filter_range[index] < minval:
+    if contour_range[index] < minval:
         rise = True
-    print(f"filter_range: {filter_range}")
+    print(f"contour_range: {contour_range}")
 
 
 def loop():
     while True:
-        if vary_filter_range:
+        if vary_contour_range:
             vary_range()
 
         ret, frame = cap.read()
@@ -53,7 +57,8 @@ def loop():
         gray = frame.copy()
         gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
         # apply binary color filter to detect dark regions better
-        _, thresh = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)
+        _, thresh = cv2.threshold(gray, threshold_range[0], threshold_range[1],
+                                  cv2.THRESH_BINARY_INV)
         cv2.imshow("thresh", thresh)
 
         # find contours of the binary image
@@ -65,7 +70,7 @@ def loop():
         global area
         for contour in contours:
             area = cv2.contourArea(contour)
-            if filter_range[0] < area < filter_range[1]:
+            if contour_range[0] < area < contour_range[1]:
                 M = cv2.moments(contour)
                 cx = int(M["m10"] / M["m00"])
                 cy = int(M["m01"] / M["m00"])
